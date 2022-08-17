76121ce674b5f0f5324253132dd25a70a984e43e
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-17 15:30:43 -0300

    Resolução nono requisito

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -42,14 +42,49 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  it('Verificar se a função retorna undefined quando não recebe nenhum parâmetro', () => {
+    expect(getCharacter()).toBeUndefined();
+  });
+  it('Verificar se a função retorna o objeto correto quando Arya é chamado', () => {
+    const arya = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    }
+    expect(getCharacter('Arya')).toMatchObject(arya);
+  });
+  it('Verificar se a função retorna o objeto correto quando Brienne é chamado', () => {
+    const brienne = {
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    }
+    expect(getCharacter('Brienne')).toMatchObject(brienne);
+  });
+  it('Verificar se a função retorna o objeto correto quando Melissandre é chamado', () => {
+    const melissandre = {
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.'
+      , 'For The Night Is Dark And Full Of Terrors.'],
+    }
+    expect(getCharacter('Melissandre')).toMatchObject(melissandre);
+  });
+  it('Verificar se a função é case sensitive', () => {
+    const arya = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    }
+    expect(getCharacter('arya')).toMatchObject(arya);
+    expect(getCharacter('arya')).toMatchObject(arya);
+    expect(getCharacter('ARYA')).toMatchObject(arya);
+    expect(getCharacter('aRyA')).toMatchObject(arya);
+    expect(getCharacter('ArYa')).toMatchObject(arya);
+    expect(getCharacter('aRYa')).toMatchObject(arya);
+    expect(getCharacter('AryA')).toMatchObject(arya);
+  });
+  it('Verificar se a função retorna undefined quando recebe um parâmetro que não está na tabela', () => {
+    expect(getCharacter('Rodrigo')).toBeUndefined();
   });
 });
