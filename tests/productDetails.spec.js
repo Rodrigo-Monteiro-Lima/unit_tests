0e29ce002ff75a60cabcdb4d4e64441a0389cdfe
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-17 15:22:48 -0300

    Resolução sexto requisito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,28 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('Verificar se productDetails é função ', () => {
+    expect(typeof productDetails).toBe('function');
+  });
+  it('Verificar se a função retorna um array', () => {
+    expect(typeof productDetails('Álcool gel', 'Máscara')).not.toBe('array');
+  });
+  it('Verificar se o array retorna dois itens', () => {
+    expect(productDetails('Álcool gel', 'Máscara').length).toBe(2);
+  });
+  it('Verificar se os dois itens dentro do array são objetos', () => {
+    expect(typeof productDetails('Álcool gel', 'Máscara')[0]).toBe('object');
+    expect(typeof productDetails('Álcool gel', 'Máscara')[1]).toBe('object');
+  });
+  it('Verificar se os objetos são diferentes entre si', () => {
+    const product = productDetails('Álcool gel', 'Máscara');
+    expect(product[0]).not.toMatchObject(product[1]);
+    expect(product[1]).not.toMatchObject(product[0]);
+  });
+  it('Verificar se os dois productIds terminam com 123', () => {
+    const product1 = productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123');
+    const product2 = productDetails('Alcool gel', 'Máscara')[1].details.productId.endsWith('123');
+    expect(product1).toBe(true);
+    expect(product2).toBe(true);
   });
 });
