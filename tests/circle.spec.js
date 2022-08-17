ad9938c13b74ffde89d4fb8f2efdb069f40be67e
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-17 15:14:13 -0300

    Resolução quarto requisito

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,15 +22,29 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+  it('Verificar se a função retorna undefined caso recebe um parâmetro que não seja número', () => {
+    expect(circle('1')).toBeUndefined();
+    expect(circle('Rodrigo')).toBeUndefined();
+    expect(circle('')).toBeUndefined();
+    expect(circle(' ')).toBeUndefined();
+  });
+  it('Verificar se a função retorna um objeto', () => {
+    expect(typeof circle(3)).toBe('object');
+  });
+  it('Verificar se a função retorna um objeto com 3 propriedades', () => {
+    expect(Object.keys(circle(2)).length).toBe(3);
+  });
+  it('Verificar se a função retorna undefined quando não recebe nenhum parâmetro', () => {
+    expect(circle()).toBeUndefined();
+  });
+  it('Verificar se a função retorna a circunferência correta para um circulo de raio 2', () => {
+    expect(circle(2)).toHaveProperty('circumference', 12.56);
+  });
+  it('Verificar se a função retorna a área correta para um circulo de raio 3', () => {
+    expect(circle(3)).toHaveProperty('area', 28.259999999999998);
+  });
+  it('Verificar se a função retorna os dados correos para um circulo de raio 3', () => {
+    const result = { radius: 3, area: 28.259999999999998, circumference: 18.84 };
+    expect(circle(3)).toMatchObject(result);
   });
 });
